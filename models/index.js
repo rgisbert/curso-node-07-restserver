4f41b7db0501fb24812f4076d13a1dbfd30decbb
Ramón G. Gisbert <gisbert.rg@gmail.com>
2022-09-06 18:33:10 +0200

    Modelo Categoría

---
+++ models/index.js
@@ -0,0 +1,11 @@
+const Categoria = require('./categoria.js');
+const Role = require('./role.js');
+const Server = require('./server.js');
+const Usuario = require('./usuario.js');
+
+module.exports = {
+  Categoria,
+  Role,
+  Server,
+  Usuario,
+};
